653ae5046806e82212412a96ddfa5e58e96f5a7b
Tyler Blalock <twblalock@gmail.com>
2014-05-07 02:07:22 -0700

    a bit of refactoring

--- sideways.lisp
+++ sideways.lisp
@@ -1,18 +1,25 @@
-(let ((file (open (second *posix-argv*))))
-  (let ((maxlen 0))
-    (let ((lines nil))
-      (loop for line = (read-line file nil)
-	    while line do
-	    (setf maxlen (max maxlen (length line)))
-	    (cond ;; There must be a better way
-	     ((null lines) (setf lines (list line)))
-	     ((null (cdr lines)) (setf (cdr lines) (list line)))
-	     (t (setf (cdr (last lines)) (list line)))))
-      (close file)
-      (loop while (>= maxlen 0) do
-	    (loop for line in lines do
-		  (if (> (length line) maxlen)
-		      (princ (elt line maxlen))
-		    (princ " ")))
-	    (format t "~%" nil) ;; TODO there must be a better way, but princ doesn't work...
-	    (decf maxlen)))))
+(defun readfile (filename)
+  (let ((lines nil) (file (open filename)))
+    (loop for line = (read-line file nil)
+	  while line do
+	  (cond ;; There must be a better way
+	   ((null lines) (setf lines (list line)))
+	   ((null (cdr lines)) (setf (cdr lines) (list line)))
+	   (t (setf (cdr (last lines)) (list line)))))
+    (close file)
+    lines))
+
+(defun maxlength (lines)
+  (reduce #'max (mapcar #'(lambda (x) (length x)) lines)))
+
+(defun sideways (lines)
+  (if (= (length lines) 0)
+      nil
+    (loop for i from (- (maxlength lines) 1) downto 0 do
+	  (loop for line in lines do
+		(if (> (length line) i)
+		    (princ (elt line i))
+		  (princ " ")))
+	  (format t "~%" nil)))) ;; TODO there must be a better way, but princ doesn't work...
+
+(sideways (readfile (second *posix-argv*)))
